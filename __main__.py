ef2ace6f229b56cc0ee6bd0c6151c1d3bd381291
RobertoHiroshi <mrhiroshiroberto@yahoo.com.br>
2022-12-08 17:05:35 -0300

    first test

--- __main__.py
+++ __main__.py
@@ -4,6 +4,19 @@ import matplotlib.pyplot as plt
 pipeline = keras_ocr.pipeline.Pipeline()
 
 images = [
-    keras_ocr.tools.read(img) for img in ['/content/Image1.png',
-                                          '/content/Image2.png',]
+    keras_ocr.tools.read(img) for img in ['/content/BoletoBancario.png',]
 ]
+
+# generate text predictions from the images
+prediction_groups = pipeline.recognize(images)
+
+# plot the text predictions
+fig, axs = plt.subplots(nrows=len(images), figsize=(10, 20))
+for ax, image, predictions in zip(axs, images, prediction_groups):
+    keras_ocr.tools.drawAnnotations(image=image, 
+                                    predictions=predictions, 
+                                    ax=ax)
+
+predicted_image = prediction_groups[1]
+for text, box in predicted_image:
+    print(text)
